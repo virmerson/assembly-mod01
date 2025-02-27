682d596a40dbe95b9109ca3db796fba49f92957c
@virmerson <virmerson@htcursos.com>
2025-02-27 20:42:11 +0000

    feat: :sparkles: aula 02

---
+++ page2.js
@@ -0,0 +1,16 @@
+
+const btnSave = document.getElementById("btnSave")
+
+btnSave.addEventListener("click", ()=>{
+
+    const fieldName = document.getElementById("fieldName")
+    alert(fieldName.value)
+
+    const fieldEmail = document.getElementById("fieldEmail")
+    alert(fieldEmail.value)
+
+    const fieldAge = document.getElementById("fieldAge")
+    alert (fieldAge)
+
+})
+
