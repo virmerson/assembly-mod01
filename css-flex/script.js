48474b3cb859127fafd3a75582d05df6a88c4612
Virmerson <virmerson.dev@gmail.com>
2025-03-27 21:30:20 +0000

    feat: add remove and update

--- css-flex/script.js
+++ css-flex/script.js
@@ -1,26 +1,66 @@
-const arrGoal = [];
-
+const arrGoal = []
+let indexEdit = -1
 const goalList= document.getElementById("goalList");
 const btnAdd= document.getElementById("btnAdd");
+const btnCancel= document.getElementById("btnCancel");
 const inpDesc = document.getElementById("inpDesc");
 const inpHours = document.getElementById("inpHours");
 
+btnCancel.addEventListener ('click', ()=>{
+    resetForm()
+})
+
 btnAdd.addEventListener('click', ()=>{
     const goal = { description:inpDesc.value , hours: inpHours.value} 
-    arrGoal.push(goal)
+   
+    if (indexEdit==-1){
+        arrGoal.push(goal)
+    }else{
+       arrGoal[indexEdit] =  goal
+
+    }
+   
     renderGoalList()
-    clearForm()
+    resetForm()
 })
 
 const renderGoalList=()=>{
-   const arrGoalRendered = arrGoal.map( (goal) => `<div> ${goal.description} | ${goal.hours}</div>`   )
+   const arrGoalRendered = arrGoal.map( (goal, index) => 
+            `<div>
+                    <div> ${goal.description} </div>
+                    <div> ${goal.hours} </div> 
+                    <div> <button onclick='goalDelete(${index})'> Delete </button> </div>
+                    <div> <button onclick='goalEdit(${index})'> Edit </button> </div>
+            </div>`   )
    goalList.innerHTML =  arrGoalRendered.join("")
 }
 
-const clearForm=()=>{
+const goalDelete=(index)=>{
+   arrGoal.splice(index, 1)
+   renderGoalList()
+}
+
+const goalEdit=(index)=>{
+    indexEdit = index
+
+    let selectedGoal = arrGoal[index]
+    
+    inpDesc.value =  selectedGoal.description
+    inpHours.value =  selectedGoal.hours
+
+    btnCancel.className = 'visible'
+    btnAdd.innerText = 'Update'
+
+}
+
+const resetForm=()=>{
+    indexEdit = -1
     inpDesc.value=""
     inpHours.value=""
+    btnAdd.innerText ='Add'
+    btnCancel.className= 'invisible'
 }
+
 
 
 
